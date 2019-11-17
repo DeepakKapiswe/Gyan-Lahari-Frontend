22669eefc929d30fcf3c69132edc407fe9caf028
deepakkapiswe <deepakkapiswe@gmail.com>
2019-11-17 13:46:55 +0530

    add support for fetching data from own Api

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import useFetch from '../../Common/UseFetchSuspense';
 
 export default function Users(props) {
-    return (<p1>User's Page <h1>{props.userId}</h1></p1>);
+    // let url ='http://localhost:7000/item/';
+    let url ='http://192.168.43.28:7000/item/';
+    // let url2 = 'https://jsonplaceholder.typicode.com/todos/1';
+    const result = useFetch(url); // { mode: 'no-cors'});
+    return (
+      <>
+      
+    <h1>{result.itemText}</h1>
+    <h1>{result.itemId}</h1>
+<h1> SuccessFull</h1>
+      </>);
   }
   
