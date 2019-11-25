9c4c4cbb5884b10b4d1ab2051934d449ac495eec
deepakkapiswe <deepakkapiswe@gmail.com>
2019-11-25 18:19:36 +0530

    add data fetching from database

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -2,16 +2,17 @@ import React from 'react';
 import useFetch from '../../Common/UseFetchSuspense';
 
 export default function Users(props) {
-    let url ='http://localhost:7000/item/';
+    let url ='http://localhost:7000/user';
+    let url2 ='http://192.168.43.28:7000/';
     // let url ='http://192.168.43.28:7000/item/';
-    // let url2 = 'https://jsonplaceholder.typicode.com/todos/1';
-    const result = useFetch(url); // { mode: 'no-cors'});
+    let url4 = 'https://jsonplaceholder.typicode.com/todos/1';
+    const result = useFetch(url2); // { mode: 'no-cors'});
+    const items = result.map((item)=> <h2>{item.m} </h2>);
     return (
       <>
       
-    <h1>{result.itemText}</h1>
-    <h1>{result.itemId}</h1>
-<h1> SuccessFull</h1>
+    {/* <h1>{result.itemId}</h1> */}
+    {items}
       </>);
   }
   
