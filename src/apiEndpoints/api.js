0b7c37745065fef6998982f6362372b5ee0b5d75
deepakkapiswe <deepakkapiswe@gmail.com>
2020-08-14 17:22:48 +0530

    change resource fetching domains

--- src/apiEndpoints/api.js
+++ src/apiEndpoints/api.js
@@ -1,14 +1,14 @@
 
-export const url_login = 'https://www.gyanlahari.com/api/login';
-export const url_addDistributor = 'https://www.gyanlahari.com/api/addDistributor/';
-export const url_addUser = 'https://www.gyanlahari.com/api/addUser/';
-export const url_bulkDistributionList = 'https://www.gyanlahari.com/api/bulkDistributionList/';
-export const url_bulkExpiryList = 'https://www.gyanlahari.com/api/bulkExpiryList/';
-export const url_distributionList = 'https://www.gyanlahari.com/api/distributionList/';
-export const url_expiryList = 'https://www.gyanlahari.com/api/expiryList/';
-export const url_searchSubscriber = 'https://www.gyanlahari.com/api/searchSubscriber/';
-export const url_recentlyAddedSubscribers = 'https://www.gyanlahari.com/api/recentlyAddedSubscribers/';
-export const url_updateDistributor = 'https://www.gyanlahari.com/api/updateDistributor/';
-export const url_updateSubscriber = 'https://www.gyanlahari.com/api/updateSubscriber/';
-export const url_getAllDistributor = 'https://www.gyanlahari.com/api/getAllDistributor/';
-export const url_getAllSubscribers = 'https://www.gyanlahari.com/api/getAllSubscribers/';
+export const url_login = 'https://gyanlahari.com/api/login/';
+export const url_addDistributor = 'https://gyanlahari.com/api/addDistributor/';
+export const url_addUser = 'https://gyanlahari.com/api/addUser/';
+export const url_bulkDistributionList = 'https://gyanlahari.com/api/bulkDistributionList/';
+export const url_bulkExpiryList = 'https://gyanlahari.com/api/bulkExpiryList/';
+export const url_distributionList = 'https://gyanlahari.com/api/distributionList/';
+export const url_expiryList = 'https://gyanlahari.com/api/expiryList/';
+export const url_searchSubscriber = 'https://gyanlahari.com/api/searchSubscriber/';
+export const url_recentlyAddedSubscribers = 'https://gyanlahari.com/api/recentlyAddedSubscribers/';
+export const url_updateDistributor = 'https://gyanlahari.com/api/updateDistributor/';
+export const url_updateSubscriber = 'https://gyanlahari.com/api/updateSubscriber/';
+export const url_getAllDistributor = 'https://gyanlahari.com/api/getAllDistributor/';
+export const url_getAllSubscribers = 'https://gyanlahari.com/api/getAllSubscribers/';
