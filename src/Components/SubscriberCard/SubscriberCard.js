fff18eacfc8b917253b9575fed8e51ad5478a03a
deepakkapiswe <deepakkapiswe@gmail.com>
2021-06-19 07:52:26 +0530

    fix subscriber card according to new view of subscriptions
    Signed-off-by: deepakkapiswe <deepakkapiswe@gmail.com>

--- src/Components/SubscriberCard/SubscriberCard.js
+++ src/Components/SubscriberCard/SubscriberCard.js
@@ -11,13 +11,15 @@ import Collapse from '@material-ui/core/Collapse';
 import Divider from '@material-ui/core/Divider';
 import { navigate } from '@reach/router';
 import { useSaveNextLocation } from '../../Hooks/SaveLocation';
+import Box from '@material-ui/core/Box';
+
 
 
 const useStyles = makeStyles(({ breakpoints, spacing, shadows }) => ({
   card: {
     width: '100%',
     maxWidth: 500,
-    borderRadius: spacing(2), // 16px
+    // borderRadius: spacing(2), // 16px
     transition: '0.3s',
     // boxShadow: '0px 14px 80px rgba(34, 35, 58, 0.2)',
     boxShadow: shadows[20],
@@ -28,18 +30,29 @@ const useStyles = makeStyles(({ breakpoints, spacing, shadows }) => ({
     alignItems: 'space-around',
     paddingLeft: 3,
     paddingRight: 3,
-    background:
-      'linear-gradient(34deg, rgba(55,16,83,1) 0%, rgba(162,73,190,1) 29%, rgba(33,16,83,1) 92%)',
+    // background:
+    //       'rgb(247, 249, 250)',
+    backgroundColor: 'rgb(245, 247, 235)',
+    // background:
+    //   'linear-gradient(34deg, rgba(55,16,83,1) 0%, rgba(162,73,190,1) 29%, rgba(33,16,83,1) 92%)',
     [breakpoints.up('sm')]: {
       textAlign: 'left',
     },
+    borderBottomLeftRadius: '20%',
+    // borderBottomRightRadius: '20%',
+    // borderTopLeftRadius: '100%',
+    borderTopRightRadius: '20%',
+    background: '#fdeeee',
+    background: '-webkit-radial-gradient(top left, #fdeeee, #e8c89b)',
+    background: '-moz-radial-gradient(top left, #fdeeee, #e8c89b)',
+    background: 'radial-gradient(ellipse at top left, #fdeeee, #e8c89b);',
   },
   cardSmall: {
     width: '100%',
     minWidth: 85,
     maxWidth: 100,
 
-    borderRadius: spacing(0.9), // 16px
+    // borderRadius: spacing(0.9), // 16px
     transition: '0.3s',
     boxShadow: '0px 14px 80px rgba(34, 35, 58, 0.2)',
     position: 'relative',
@@ -52,23 +65,30 @@ const useStyles = makeStyles(({ breakpoints, spacing, shadows }) => ({
     paddingRight: 0,
     paddingTop: 0,
     paddingBottom: 0,
-    // background: 'rgb(248,255,16)',
+    background:
+          'rgb(247, 249, 250)',
     background:
       'linear-gradient(146deg, rgba(248,255,16,0.7077205882352942) 1%, rgba(254,255,254,1) 20%, rgba(194,247,143,1) 65%)',
     [breakpoints.up('sm')]: {
       textAlign: 'center',
     },
+    borderBottomLeftRadius: '30%',
+    borderTopRightRadius: '20%',
+    // borderBottomRightRadius: '20%',
+    // borderTopLeftRadius: '20%',
+    // background: 'radial-gradient(ellipse at center, #FFFFFF, #F0EBE4)',
   },
   overline: {
     lineHeight: 1.5,
-    color: '#ffffff',
+    // color: '#ffffff',
+    color: 'rgb(0 0 0)',
     fontWeight: '700',
     fontSize: '1.1rem',
     opacity: 0.8,
   },
   heading: {
     fontWeight: '700',
-    color: '#ffffff',
+    color: 'rgba(113, 11, 11,1)',
     letterSpacing: 0.5,
     fontSize: '1.5rem',
   },
@@ -81,7 +101,8 @@ const useStyles = makeStyles(({ breakpoints, spacing, shadows }) => ({
     lineHeight: 1.5,
     fontWeight: '900',
     opacity: 0.95,
-    color: '#220a70',
+    // color: '#220a70',
+    color: 'rgba(113, 11, 11,1)',
     fontSize: '1.1rem',
   },
   button: {
@@ -89,7 +110,7 @@ const useStyles = makeStyles(({ breakpoints, spacing, shadows }) => ({
     borderRadius: 9,
     paddingLeft: 10,
     paddingRight: 10,
-    color: '#ffffff',
+    // color: '#ffffff',
     textTransform: 'none',
     width: '100%',
     '&:hover': {
@@ -98,6 +119,28 @@ const useStyles = makeStyles(({ breakpoints, spacing, shadows }) => ({
     [breakpoints.up('sm')]: {
       width: 'auto',
     },
+    background: 'radial-gradient(ellipse at center, #FFFFFF, #F0EBE4)',
+  },
+  messageBox :{
+    textAlign: 'center',
+    flexGrow: 1,
+    fontSize: '1rem',
+    fontWeight: 'bold',
+    color: 'rgba(113, 11, 11,1)',
+    padding: '10px 5px 10px 5px',
+    margin: '10px 0px 10px 0px',
+    fontWeight: 'bold',
+    color: '#06044d',
+    background: '#FFFFFF',
+    background: '-webkit-radial-gradient(center, #FFFFFF, #F0EBE4)',
+    background: '-moz-radial-gradient(center, #FFFFFF, #F0EBE4)',
+    background: 'radial-gradient(ellipse at center, #FFFFFF, #F0EBE4)',
+    borderBottomLeftRadius: '20px',
+    borderTopRightRadius:   '20px',
+  },
+  gridCard : {
+    paddingTop : '10px',
+
   },
   
 }));
@@ -133,6 +176,7 @@ export default function SubscriberCard(props) {
               spacing={1}
               direction="row"
               alignItems="flex-end"
+              // alignItems="flex-start"
             >
               <Grid item xs container>
                 <Grid container
@@ -202,28 +246,36 @@ export default function SubscriberCard(props) {
                       </CardContent>
                     </Card>
                   </Grid>
-                  <Grid item>
-                    <Card className={styles.cardSmall}>
-                      <CardContent className={styles.cardcontent}>
-                        <Typography variant={'subtitle'} gutterBottom>
-                          DC : {sD.subDistId}
-                        </Typography>
-                      </CardContent>
-                    </Card>
-                  </Grid>
-                  <Grid item>
-                    <Card className={styles.cardSmall}>
-                      <CardContent className={styles.cardcontent}>
-                        <Typography variant={'subtitle'} gutterBottom >
-                          {sD.subSubscriptionType} Year <br />
-                          {sD.subStartVol}-{sD.subEndVol}
-                        </Typography>
-                      </CardContent>
-                    </Card>
-                  </Grid>
+                  {sD.currPlan && <> 
+                          <Grid item>
+                          <Card className={styles.cardSmall}>
+                            <CardContent className={styles.cardcontent}>
+                              <Typography variant={'subtitle'} gutterBottom>
+                                DC : {sD.currPlan.subdistid}
+                              </Typography>
+                            </CardContent>
+                          </Card>
+                        </Grid>
+                        <Grid item>
+                          <Card className={styles.cardSmall}>
+                            <CardContent className={styles.cardcontent}>
+                              <Typography variant={'subtitle'} gutterBottom >
+                                {sD.currPlan.subplan} Year <br />
+                                {sD.currPlan.substartvol}-{sD.currPlan.subendvol}
+                              </Typography>
+                            </CardContent>
+                          </Card>
+                        </Grid> 
+                      </>
+                  }
                 </Grid>
               </Grid>
             </Grid>
+            {! sD.currPlan  && <Box className={styles.messageBox}>
+                        <Typography variant="h5" >
+                        *** No Active Subscriptions ***
+                        </Typography>
+            </Box>}
           </CardContent>
           { props.noActionButtons === undefined && 
           <CardActions disableSpacing>
@@ -250,13 +302,93 @@ export default function SubscriberCard(props) {
             <CardContent>
               <Grid item>
                 <Typography className={styles.overline} gutterBottom variant={'body1'}>
-                  Subscription Status: <br /> Expiry here
-              </Typography>
+                  Subscriptions 
+                </Typography>
+                  <Divider variant="middle" />
+                  <Typography variant="subtitle1">
+                                    Current Plan : 
+                        </Typography>
+                  {sD.currPlan 
+                        ?   <Subscription subscriptionDetails={sD.currPlan} />
+                        :   <Typography 
+                              variant="subtitle1" align="right"
+                              >
+                              <Divider variant="middle" />
+                              <i>
+                                    No Plan Currently Active! <br/>
+                                    Please Renew <br/>
+                                  <span>
+                                     Feel free to Contact: +91-9155950505
+                                    </span> 
+                              </i>
+                              <Divider variant="middle" />
+                                </Typography>
+                  } 
+                  <Grid container justify="space-between">
+                    <Grid item>
+                  <Typography variant="subtitle1">
+                                    Upcoming Plans :
+                  </Typography>
+                      
+                    </Grid>
+                    <Grid item>
+                  <Typography variant={'body1'} align="right" >
+                    {sD.upcomingPlans && sD.upcomingPlans.length} 
+                  </Typography>
+
+                    </Grid>
+                  </Grid>
+                 { sD.upcomingPlans 
+                    ?  <>
+                      {sD.upcomingPlans.map(sub => (
+                        <Grid item  className={styles.gridCard}>
+                            <Subscription subscriptionDetails={sub} />
+                        </Grid>))
+                      }
+                    </>
+                    :   <Typography variant={'body1'} align="right">
+                          <Divider variant="middle" />
+                                    <i>You have no Upcoming Plans <br/>
+                                    Please Renew Subscriptions before <br/>
+                                    expiry to avoid discontinuity <br/>
+                                    Feel free to Contact: +91-9155950505</i>
+                          <Divider variant="middle" />
+                                </Typography>
+
+                    }
+                    <Grid container justify="space-between">
+                    <Grid item>
+                      <Typography variant="subtitle1" >
+                                        Expired Plans :
+                      </Typography>
+                    </Grid>
+                    <Grid item>
+                      <Typography variant="subtitle1" align="right">
+                                        {sD.oldPlans && sD.oldPlans.length}
+                      </Typography>
+                    </Grid>
+                    </Grid>
+
+                  { sD.oldPlans 
+                    ?  <Grid container spacing={5}>
+                      {sD.oldPlans.map(sub => (
+                        <Grid item  className={styles.gridCard} >
+                            <Subscription subscriptionDetails={sub} />
+                        </Grid>))
+                      }
+                    </Grid>
+                    :   <Typography variant={'body1'} align="right" >
+                              <Divider variant="middle" />
+                                    <i>No Plans Expired</i> 
+                              <Divider variant="middle" />
+                        </Typography>
+
+                    }
                 <Divider variant="middle" />
               </Grid>
-              <Typography paragraph className={styles.overline} gutterBottom variant={'body1'}>
+              {/* <Typography paragraph className={styles.overline} gutterBottom variant={'body1'}>
                 Contact Person: <br />Distributor Name And Number Here
-            </Typography>
+            </Typography> */}
             </CardContent>
           </Collapse>
         </Card>
@@ -265,3 +397,51 @@ export default function SubscriberCard(props) {
       </>
     )
   };
+
+function Subscription (props) {
+  const s = props.subscriptionDetails;
+  const styles = useStyles();
+  return (
+    <Card variant="outlined" classname={styles.subscriptiopnCard}>
+
+                <Typography variant="button" 
+                  align="left" color="textSecondary"
+                >
+                  {makeColumn ("Plan",<>{s.subplan} Years</>)}
+                  <Divider variant="fullWidth" />
+                  {makeColumn("Valid for Volume Number",<>{s.substartvol} --{'>'} {s.subendvol}</>)}
+                  <Divider variant="fullWidth" />
+                  {makeColumn("Subscription Id", s.subscriptionid)}
+                  <Divider variant="fullWidth" />
+                  {makeColumn ("Date" , formatDate(s.subcreatedat))}
+                  <Divider variant="fullWidth" />
+                  {makeColumn ("Slip No.",s.subslipnum)} 
+                  <Divider variant="fullWidth" />
+                  {makeColumn ("Type", s.subtype)} 
+                </Typography>
+    </Card>
+
+  );
+}
+
+const formatDate = (dateString) => {
+  const options = { year: "numeric", month: "long", day: "numeric" }
+  return new Date(dateString).toLocaleDateString(undefined, options)
+}
+
+const makeColumn = (str, comp) => {
+  return <Grid container justify="space-between"  alignItems="baseline">
+            <Grid item>
+              <Typography variant="button" color="textSecondary" >
+                <i>{str}</i>
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body2" align="right" >
+                <b>
+                  {comp}
+                  </b>
+              </Typography>
+            </Grid>
+          </Grid>
+}
